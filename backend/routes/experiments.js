d7b31bf051be6825dd7077b8f8436dee742512bd
Viczenith <viczenithgodwin@gmail.com>
2025-07-24 18:37:12 +0100

    fixes again

--- backend/routes/experiments.js
+++ backend/routes/experiments.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express    = require('express');
 const Experiment = require('../models/Experiment');
 const { protect, authorize } = require('../middleware/auth');
